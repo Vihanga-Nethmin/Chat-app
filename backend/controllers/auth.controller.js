bc32ad34db39025f6b988f05359c132df65a995a
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-06-08 15:44:51 +0530

    sign up a user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,46 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+
 export const signup= async(req,res) =>{
     try{
         const {fullName,username,password,confirmPassword,gender}= req.body;
+
+        if(password !== confirmPassword){
+            return res.status(400).json({error:"Password don't match"})
+        }
+        const user =await User.findOne({username})
+        if(user){
+            return res.status(400).json({error:"Username already exists"})
+        }
+
+           const salt = await bcryptjs.genSalt(10);
+
+        const boyProfilePic = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}&accessories=blank&top=shortHair`;
+        const girlProfilePic = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}&accessories=blank&top=longHair`;
+
+    const newUser = new User({
+        fullName,
+        username,
+        password,
+        gender,
+        profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+    })
+
+    await newUser.save();
+    res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username:newUser.username,
+        profilePic:newUser.profilePic
+    })
+
     }
-    catch{}
+    catch(error){
+        console.log("error in signup controller",error.message);
+        res.status(500).json({error:"Internal Server Error"})
+
+    }
 }
 
 export const login=(req,res) =>{
