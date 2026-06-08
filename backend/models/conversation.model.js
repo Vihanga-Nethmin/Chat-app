d06a2924b8735bba6e5e0e267266176d46a751d0
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-06-08 22:28:42 +0530

    create Message routes Get message endpoint

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const conversationSchema =new mongoose.Schema({
     participants:[
         {
-            typeof:mongoose.Schema.Types.ObjectId,
+            type:mongoose.Schema.Types.ObjectId,
             ref:"User",
         }
     ],
