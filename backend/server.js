1af5b7cfc024115de66c8547bc21982eee92229f
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-06-08 11:26:59 +0530

    create authroute

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
-const express =require("express")
+import express from "express";
+import dotenv from "dotenv";
 
-const app=express();
-app.get("/",( req,res)=>{
-    res.set("Hello World");
-}
-)
+const app = express();
 
-app.listen ( 5000,() => console.log("server Running on port 5000"));
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/api/auth",authRoutes)
+
+
+app.listen(PORT, () => console.log(`server Running on port ${PORT}`));
