845a69c0f137236f3228df62666e022e309a6b2f
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-06-09 22:31:52 +0530

    create MessageContainer Component

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
+// Home.jsx - correct path
+import MessageContainer from "../../components/sidebar/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
     <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-<Sidebar/>
+      <Sidebar />
+      <MessageContainer />
     </div>
   );
 };
