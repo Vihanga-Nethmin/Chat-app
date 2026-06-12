be4e2a47a888aeab7d0f0d847cb6a4ace786c487
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-06-12 21:10:47 +0530

    implementing socket.io,create online status,create realtime messages,create Messages notification sound

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId } from "../socket/socket.js";
+import { io } from '../socket/socket.js';
 
 export const sendMessage = async (req, res) => {
     try {
@@ -27,7 +29,22 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id);
         }
 
+
+
+
         await Promise.all([conversation.save(), newMessage.save()]);
+
+
+       const receiverSocketId= getReceiverSocketId(receiverId);
+       if(receiverSocketId){
+        io.to(receiverSocketId).emit("newMessage",newMessage)
+
+       }
+
+
+
+
+
 
         res.status(201).json(newMessage);
 
