388b760904afecd3c8a95e2429e224898e88b435
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-06-19 23:45:56 +0530

    deploy

--- frontend/src/hooks/useGetConversations.ts
+++ frontend/src/hooks/useGetConversations.ts
@@ -10,7 +10,7 @@ const useGetConversations = () => {
     const getConversations = async () => {
       setLoading(true);
       try {
-        const res = await fetch("/api/users");
+        const res = await fetch("http://172.245.220.221:5000/api/users");
         const data = await res.json();
         if (data.error) {
           throw new Error(data.error);
